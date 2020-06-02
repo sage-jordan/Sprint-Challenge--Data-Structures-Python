c05510d8182265ff04d355d426298fff040f67e3
sage-jordan <sagemjordan@gmail.com>
2020-06-01 21:33:16 -0400

    stretch

--- names/names.py
+++ names/names.py
@@ -92,6 +92,10 @@ for name_2 in names_2:
     if bst.contains(name_2):
         duplicates.append(name_2)
 
+# Stretch Goal
+""" Runtime 1.25s """
+# duplicates = [ name for name in names_1 if name in names_2 ] 
+
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
@@ -100,5 +104,4 @@ print (f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
-""" Runtime 1.25s """
-# duplicates = [ name for name in names_1 if name in names_2 ] 
+
